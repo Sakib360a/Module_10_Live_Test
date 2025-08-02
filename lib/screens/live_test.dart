7f5c1871c79b6cd97f115eef2f7cd1895761c2fe
Sakib360a <abubakkarsakib360@gmail.com>
2025-08-02 23:27:04 +0600

    Live Test Finished

--- lib/screens/live_test.dart
+++ lib/screens/live_test.dart
@@ -1,11 +1,79 @@
 import 'package:flutter/material.dart';
 class LiveTest extends StatelessWidget {
-  const LiveTest({super.key});
+   LiveTest({super.key});
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController salaryController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+    appBar: AppBar(
+      title: Text('Add Employee',style: TextStyle(fontSize: 25),),
+    ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10,vertical: 0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: nameController,
+              style: TextStyle(fontSize: 25),
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                labelStyle: TextStyle(fontSize: 20),
+                border: UnderlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            TextFormField(
+              controller: ageController,
+              keyboardType: TextInputType.number,
+              style: TextStyle(fontSize: 25),
+              decoration: const InputDecoration(
+                labelText: 'Age',
+                labelStyle: TextStyle(fontSize: 20),
+                border: UnderlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            TextFormField(
+              controller: salaryController,
+              style: TextStyle(fontSize: 25),
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Salary',
+                labelStyle: TextStyle(fontSize: 20),
+                border: UnderlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 40,),
+            SizedBox(
+                height: 55,
+                width: 170,
+                child: ElevatedButton(onPressed: (){
+                  String name = nameController.text.trim();
+                  String age = ageController.text.trim();
+                  String salary = salaryController.text.trim();
 
+                  if (name.isNotEmpty && age.isNotEmpty && salary.isNotEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("✅ Employee Added")),
+                    );
+
+                    print('Name: $name');
+                    print('Age: $age');
+                    print('Salary: $salary');
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("⚠️ Please fill all fields")),
+                    );
+                  }
+                }, child: Text('Add Employee',style:
+                TextStyle(fontSize: 18),)))
+          ],
+        ),
+      ),
     );
   }
 }
